c33d65e63f1b9dbe6379dc266f13cbe5817bc8c9
dee <dee@opencitieslab.com>
2022-05-14 19:17:56 +0200

    create a a template response for status checker

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
+templates = Jinja2Templates(directory="templates")
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/", response_class=HTMLResponse)
+async def index(request: Request):
+    context = {'request': request }
+    return templates.TemplateResponse(("index.html", context)
 
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+#
+# @app.get("/hello/{name}")
+# async def say_hello(name: str):
+#     return {"message": f"Hello {name}"}
