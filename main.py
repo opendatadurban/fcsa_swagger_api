e51d4049f1c506f0cab126b95f1d1486afd6d20c
dee <dee@opencitieslab.com>
2022-05-14 19:34:31 +0200

    Template response has been created for context

--- main.py
+++ main.py
@@ -7,14 +7,9 @@ app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
 
-@app.get("/", response_class=HTMLResponse)
-async def index(request: Request):
-    context = {'request': request }
-    return templates.TemplateResponse(("index.html", context))
+@app.get('/')
+def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000)
-#
-# @app.get("/hello/{name}")
-# async def say_hello(name: str):
-#     return {"message": f"Hello {name}"}
+if __name__ == '__main__':
+    uvicorn.run(app, host='127.0.0.1', port=8000, debug=True)
