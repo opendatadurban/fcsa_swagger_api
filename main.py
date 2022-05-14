0e263bb7ef133c065ebefb428b0888225951e578
dee <dee@opencitieslab.com>
2022-05-14 20:51:32 +0200

    testing async routes

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 import uvicorn
 
 app = FastAPI()
-
 templates = Jinja2Templates(directory="templates")
 
+
+
 @app.get('/')
-def index(request: Request):
+async def index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
 if __name__ == '__main__':
