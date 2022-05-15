6850cf6b249972f532acd9d16a144fbb64bcad88
dee <dee@opencitieslab.com>
2022-05-15 20:13:04 +0200

    1. working base flow

--- api/api.py
+++ api/api.py
@@ -1,10 +1,9 @@
 from fastapi import APIRouter
 
-from status_checker.api_check_status import check_status_router
-
+from api.status_checker.api_check_status import check_status_router
 
 ApiRouter = APIRouter()
 '''
     include all routes
 '''
-ApiRouter.include_router(check_status_router.router,prefix='/status_checker')
+ApiRouter.include_router(check_status_router,prefix='/status_checker')
--- main.py
+++ main.py
@@ -1,28 +1,16 @@
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 import uvicorn
-from api.api import ApiRouter
-
-
-
-description = """
-#### CCT API code and documentation for talking to the City's Service Request API
-"""
-
-'''
-import all routes to be used in app(FASTAPI)
-'''
-app = ApiRouter(title='cct-api', version=1.0, description=description,)
-
-templates = Jinja2Templates(directory="templates")
-'''
-    import all api routes that have been created
-    Each route has a test that has been created for them
-'''
-# @app.get('/')
-# async def index(request: Request):
-#     return templates.TemplateResponse("index.html", {"request": request})
 
 from api.api import ApiRouter
+
+app = FastAPI(title='cct-api', version=1.0, description="Well done",)
+app.include_router(ApiRouter,tags=['Api'])
+
+'''
+    creating 
+'''
 if __name__ == '__main__':
     uvicorn.run(app, host='127.0.0.1', port=8000, debug=True)
+
+
